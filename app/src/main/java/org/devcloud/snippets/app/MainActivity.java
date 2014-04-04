e25b114a03010d319892de4284ffe85bfee8e3a8
Nat Welch <nat@natwelch.com>
2014-04-04 13:42:29 -0700

    rename main fragment

--- app/src/main/java/org/devcloud/snippets/app/MainActivity.java
+++ app/src/main/java/org/devcloud/snippets/app/MainActivity.java
@@ -79,7 +79,7 @@ public class MainActivity extends Activity {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-      View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+      View rootView = inflater.inflate(R.layout.fragment_post_snippet, container, false);
       return rootView;
     }
 
