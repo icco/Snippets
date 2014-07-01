45870551e9d337675ac7cfc51dca7b39250e8fb3
Nat Welch <nat@natwelch.com>
2014-07-01 08:00:34 +0000

    hmm

---
+++ app/src/main/java/org/devcloud/snippets/app/SnippetTest.java
@@ -0,0 +1,35 @@
+package org.devcloud.snippets.app;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SnippetTest {
+
+  @Before
+  public void setUp() throws Exception {
+
+  }
+
+  @After
+  public void tearDown() throws Exception {
+
+  }
+
+  @Test
+  public void testGetArrayListForAll() throws Exception {
+
+  }
+
+  @Test
+  public void testGetUuid() throws Exception {
+
+  }
+
+  @Test
+  public void testSave() throws Exception {
+
+  }
+}
