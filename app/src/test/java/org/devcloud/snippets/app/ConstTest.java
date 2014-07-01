dddd5da7b4394221ba9030b23eaafd00e327f297
Nat Welch <nat@natwelch.com>
2014-07-01 08:08:39 +0000

    blargh

---
+++ app/src/test/java/org/devcloud/snippets/app/ConstTest.java
@@ -0,0 +1,21 @@
+package org.devcloud.snippets.app;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConstTest extends TestCase {
+
+  @Before
+  public void setUp() throws Exception {
+
+  }
+
+  @Test
+  public void testIsNetworkAvailable() throws Exception {
+
+  }
+}
