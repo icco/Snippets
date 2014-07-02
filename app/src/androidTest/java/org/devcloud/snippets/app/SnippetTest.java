414553a486f3cd10d5bf810deef0cd4b54df7b88
Nat Welch <nat@natwelch.com>
2014-07-02 19:23:15 +0000

    Add test

---
+++ app/src/androidTest/java/org/devcloud/snippets/app/SnippetTest.java
@@ -0,0 +1,35 @@
+package org.devcloud.snippets.app;
+
+import junit.framework.TestCase;
+
+public class SnippetTest extends TestCase {
+
+  public void setUp() throws Exception {
+    super.setUp();
+
+  }
+
+  public void testGetCursorForAll() throws Exception {
+
+  }
+
+  public void testGetArrayListForAll() throws Exception {
+
+  }
+
+  public void testFindByUUID() throws Exception {
+
+  }
+
+  public void testGetJsonArrayForAll() throws Exception {
+
+  }
+
+  public void testGetUuid() throws Exception {
+
+  }
+
+  public void testSave() throws Exception {
+
+  }
+}
